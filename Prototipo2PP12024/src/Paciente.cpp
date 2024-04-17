e47277e701147b3614ae0d5c1ba96c0be6ddbe49
Pablo Palencia <ppalenciar1@miumg.edu.gt>
2024-04-17 10:29:50 -0600

    actualizando

--- Prototipo2PP12024/src/Paciente.cpp
+++ Prototipo2PP12024/src/Paciente.cpp
@@ -1,7 +1,72 @@
 #include "Paciente.h"
 
-Paciente::paciente()
+void Paciente::menu()
 {
-    //ctor
-}
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
 
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t |  SISTEMA GESTION PACIENTES  |"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t 1. Ingreso Pacientes"<<endl;
+	cout<<"\t\t\t 2. Despliegue Pacientes"<<endl;
+	cout<<"\t\t\t 3. Modifica Pacientes"<<endl;
+	cout<<"\t\t\t 4. Busca Pacientes"<<endl;
+	cout<<"\t\t\t 5. Borra Pacientes"<<endl;
+	cout<<"\t\t\t 6. Exit"<<endl;
+
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\tOpcion a escoger:[1/2/3/4/5/6]"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"Ingresa tu Opcion: ";
+    cin>>choice;
+
+    switch(choice)
+    {
+    case 1:
+    	do
+    	{
+    		insertar();
+    		cout<<"\n\t\t\t Agrega otro paciente(Y,N): ";
+    		cin>>x;
+		}while(x=='y'||x=='Y');
+		break;
+	case 2:
+		desplegar();
+		break;
+	case 3:
+		modificar();
+		break;
+	case 4:
+		buscar();
+		break;
+	case 5:
+		borrar();
+		break;
+	case 6:
+		exit(0);
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }while(choice!= 6);
+}
+void persona::insertar()
+{
+	system("cls");
+	fstream file;
+	cout<<"\n------------------------------------------------------------------------------------------------------------------------";
+	cout<<"\n-------------------------------------------------Agregar detalles Persona ---------------------------------------------"<<endl;
+	cout<<"\t\t\tIngresa Id Persona         : ";
+	cin>>id;
+	cout<<"\t\t\tIngresa Nombre Persona     : ";
+	cin>>name;
+	cout<<"\t\t\tIngresa Contraseña Persona   : ";
+	cin>>pass;
+	file.open("Usuarios.txt", ios::app | ios::out);
+	file<<std::left<<std::setw(15)<< id <<std::left<<std::setw(15)<< name <<std::left<<std::setw(15)<< pass << "\n";
+	file.close();
+}
